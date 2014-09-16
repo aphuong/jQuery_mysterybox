30123c97bb4a6af1397e2f1152515cb6b06036f2
Anh La Russo <Aphuong85@gmail.com>
2014-09-16 00:34:09 -0400

    edit files and add photos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+  // global variables left and top finds the position of object 
+  // parseInt returns the value as a number, without the px
+  var left = parseInt($(".blue_butterfly").css("left"), 10)
+  var top = parseInt($(".blue_butterfly").css("top"), 10)
+  var new_value
+
   $(".box_open").hide();
   $(".butterflies").hide();
   $(".step2").hide();
@@ -41,19 +47,94 @@ $(document).ready(function(){
       });
     }
 
+
   $(document).keydown(function(event) {
     if (event.which == 88) {
       event.preventDefault();
       $(".box_close").keypress();
       $(".box_close").remove();
       $(".hint").remove();
-      $(".box_open").fadeIn();
-      $(".step4").replaceWith("<h4>Butterflies!!!</h4>");
-      $(".butterflies").show();
+      $(".box_open").show();
+      $(".step4").replaceWith("<h4>Surprise, they're butterflies!</h4>");
+      $(".butterflies").fadeIn("slow");
+      $(".blue_butterfly").show();
+      $(".step5").show();
     }
   })
- 
+
+  // Down
+  $(document).keydown(function(event){
+    if (event.which == 40) {
+      event.preventDefault();
+      
+      new_value = top + 40
+      top = new_value
+
+      console.log("down arrow");
+      $(".blue_butterfly").css("top", new_value + "px")
+    }
+  });
+
+  // Up
+  $(document).keydown(function(event){
+    if (event.which == 38) {
+      event.preventDefault();
+
+      new_value = top - 40
+      top = new_value
+
+      console.log("up arrow");
+      $(".blue_butterfly").css("top", new_value + "px")
+    }
+  });
+
+  // Left
+  $(document).keydown(function(event){
+    if (event.which == 37) {
+      event.preventDefault();
+
+      new_value = left - 40
+      left = new_value
+
+      console.log("left arrow");
+      $(".blue_butterfly").css("left", new_value + "px")
+    }
+  });
+
+  // Right
+  $(document).keydown(function(event){
+    if (event.which == 39) {
+      event.preventDefault();
+
+      new_value = left + 40
+      left = new_value
+
+      console.log("right arrow");
+      $(".blue_butterfly").css("left", new_value + "px")
+    }
+  });
 });
+
+
+  // function getPosition(){
+  //   console.log $(".blue_butterfly").left;
+  // }
+
+  // getPosition();
+ // keydown functions for each of the arrow keys
+    // test that they are working with console log
+
+  // function that moves the butterfly
+    // the butterfly starts in position fixed
+    // maybe the butterfly starts at top 50% and left 50%
+    // if left key is pressed
+    // find the current left position
+    // subtract 50px
+    // set that as the new position left
+    // use css transform translate to animate 
+
+
+
 
 
 
